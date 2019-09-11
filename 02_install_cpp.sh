3d0ffbdbd584afee6fcfdd7d18e83e3505ec00c2
Peter Moran <peter.moran@vivcourt.com>
2019-09-11 13:05:20 +1000

    steps to use gcc-9

--- 02_install_cpp.sh
+++ 02_install_cpp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 
+# install gcc-9 + extras
 if ! which gcc-9 ; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test
     sudo apt-get update
@@ -7,6 +8,7 @@ if ! which gcc-9 ; then
 fi
 
 
+# install clang-9
 if ! which clang-9 ; then
     wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
     
@@ -18,3 +20,18 @@ EOF
     sudo apt-get update
     sudo apt-get install clang-9 lldb-9 lld-9 clang-format-9
 fi
+
+# use gcc-9 by default
+sudo update-alternatives --remove-all gcc 
+sudo update-alternatives --remove-all g++
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 20
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 30
+
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 30
+
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
