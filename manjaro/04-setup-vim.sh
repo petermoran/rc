fcececcee82f135e9c63933c3ee7612eb8f18707
Peter Moran <peter.john.moran@gmail.com>
2022-02-17 20:38:46 +1100

    desktop updates

--- manjaro/04-setup-vim.sh
+++ manjaro/04-setup-vim.sh
@@ -11,17 +11,16 @@ fi
 mkdir -p ${PLUGINS}
 pushd ${PLUGINS}
 
-# git clone --recursive https://github.com/python-mode/python-mode.git
 git clone --depth 1 https://github.com/w0rp/ale.git
-git clone https://github.com/morhetz/gruvbox.git
-git clone https://github.com/luochen1990/rainbow.git
-git clone https://github.com/ervandew/supertab.git
-git clone https://github.com/bfrg/vim-cpp-modern.git
-git clone https://github.com/Vimjas/vim-python-pep8-indent.git
-git clone https://github.com/drmingdrmer/vim-tabbar.git
 git clone https://github.com/junegunn/fzf.vim.git
+git clone https://github.com/NLKNguyen/papercolor-theme.git
+git clone https://github.com/nocksock/python-vim.git
+# git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/ap/vim-buftabline.git
+git clone https://github.com/bfrg/vim-cpp-modern.git
 git clone https://github.com/tpope/vim-dispatch.git
-git clone https://github.com/jremmen/vim-ripgrep.git
+git clone https://github.com/Vimjas/vim-python-pep8-indent.git
+git clone https://github.com/frazrepo/vim-rainbow
 
 # vim-peter
 ln -sf $REPO/vim-peter
